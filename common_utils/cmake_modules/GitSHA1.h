435e5caccce7ba61971b73b5f709f9d167128109
sv_91@inbox.ru <sv_91@inbox.ru>
2019-08-26 14:19:34 +0300

    Remove common_utils

--- common_utils/cmake_modules/GitSHA1.h
+++
@@ -1,9 +0,0 @@
-#ifndef GIT_SHA1_H_
-#define GIT_SHA1_H_
-
-extern const char g_GIT_SHA1[]; 
-extern const char g_GIT_REFSPEC[];
-extern const char g_GIT_IS_LOCAL_CHANGES[];
-extern const char g_GIT_DATE[];
-
-#endif //
